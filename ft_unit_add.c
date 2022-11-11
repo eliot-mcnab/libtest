6b0593152624e0c5bcbe2e8abc35054260b145c5
Eliot Mcnab <emcnab@e2r9p5.clusters.42paris.fr>
2022-11-11 09:06:00 +0100

    Finished ft_add_unit. Units can only be created this way and take in parameter a series of unit tests described by a function f_unit_tests. This function will be called when the runner runs all unit tests and has a t_unit as a parameter, allowing to perform unit tests from inside the function.

---
+++ ft_unit_add.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_unit_add.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 18:30:33 by emcnab            #+#    #+#             */
+/*   Updated: 2022/11/11 09:00:01 by emcnab           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libtest.h"
+#include "libft.h"
+#include <stdlib.h>
+
+/*
+ * @brief Creates a new unit to test.
+ *
+ * @parm unit_name (t_str): the name of the unit.
+ * @param f_unit_tests (t_tests): the tests to preform on the unit.
+ *
+ * @return (t_unit *): new unit.
+ */
+static t_unit	*ft_create_unit(t_str unit_name, t_tests f_unit_tests)
+{
+	t_unit	*unit;
+
+	unit = malloc(sizeof(*unit));
+	if (!unit)
+	{
+		return (NULL);
+	}
+	unit -> name = unit_name;
+	unit -> f_tests = f_unit_tests;
+	unit -> passed = 0;
+	unit -> failed = 0;
+	return (unit);
+}
+
+/*
+ * @brief Adds a new unit to test to a [runner].
+ *
+ * @param runner (* t_runner): the runner that will call the unit to run its
+ * 	tests.
+ * @param unit_name (t_str): the name of the unit to test.
+ * @param f_unit_tests (bool(t_unit*)): the tests to run for the unit.
+ */
+void	ft_unit_add(t_runner *runner, t_str unit_name, t_tests f_unit_tests)
+{
+	t_unit	*unit;
+	t_list	**node_current;
+
+	unit = ft_create_unit(unit_name, f_unit_tests);
+	if (!unit)
+	{
+		return ;
+	}
+	node_current = &(runner -> units_last -> next);
+	if (!(runner -> units_head))
+	{
+		node_current = &(runner -> units_head);
+	}
+	*node_current = ft_lstnew(unit);
+	if (!(*node_current))
+	{
+		return ;
+	}
+	runner -> units_last = *node_current;
+}
--- libtest.h
+++ libtest.h
@@ -6,24 +6,32 @@
 /*   By: emcnab <emcnab@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 18:10:12 by emcnab            #+#    #+#             */
-/*   Updated: 2022/11/10 18:14:27 by emcnab           ###   ########.fr       */
+/*   Updated: 2022/11/11 09:04:02 by emcnab           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBTEST_H
 # define LIBTEST_H
 
-#include "libft.h"
+# include "libft.h"
 
 typedef struct s_runner
 {
-	t_list	*units;
+	t_list	*units_head;
+	t_list	*units_last;
 }	t_runner;
 
 typedef struct s_unit
 {
-	t_list	*test;
+	t_str	name;
+	bool	(*f_tests)(struct s_unit *);
 	size_t	passed;
+	size_t	failed;
 }	t_unit;
+
+typedef bool	(*t_tests)(t_unit*);
+
+t_runner	*ft_runner_create(void);
+void		ft_unit_add(t_runner *runner, t_str unit_name, t_tests f_unit_test);
 
 #endif
